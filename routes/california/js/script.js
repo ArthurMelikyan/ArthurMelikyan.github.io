da1736f61be7bc1afe71b51a7198ff64703a9cdc
ArthurMelikyan <arthmelikyan@gmail.com>
2020-08-21 23:19:30 +0400

    california website updates

--- routes/california/js/script.js
+++ routes/california/js/script.js
@@ -4,20 +4,17 @@
 
 $(document).ready(function(){
     $(document).on('scroll', function(){
-        $("#mainNav").offset().top > 100 ? $("#mainNav").addClass("navbar-shrink") : $("#mainNav").removeClass("navbar-shrink")
+        $("#mainNav").offset().top > 80 ? $("#mainNav").addClass("navbar-shrink") : $("#mainNav").removeClass("navbar-shrink")
     });
- 
-    $("path, polyline, circle, polygon").hover(function(e) {
-        // $('#info-box').css('display','block');
-        // $('#info-box').html($(this).data('info'));
-        console.log($(this).attr('data-title'));
-      });
-      
- 
-    
-    $.get('../images/la.svg', function(svg){
-        console.log( svg );
+  
+    $(document).on('mouseenter', '#map path, #map polyline,#map circle,#map polygon', function () {
+        let m = $(this);
+        if (m.attr('title')) {
+            $(m).tooltip('show');
+        }
+    });
+    $.get('http://127.0.0.1:5500/ca.min.svg', function(svg){
+        $('#map .map_block').html(svg);
     }, 'text');
-     
-        
+ 
 });
