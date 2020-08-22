3906caedcc6a74e840e95286e3ef9147aaf7a9f4
ArthurMelikyan <arthmelikyan@gmail.com>
2020-08-22 17:49:33 +0400

    california website last changes

--- routes/california/js/script.js
+++ routes/california/js/script.js
@@ -1,20 +1,39 @@
-// ! svg  4ever
-
-
-
 $(document).ready(function(){
-    $(document).on('scroll', function(){
-        $("#mainNav").offset().top > 80 ? $("#mainNav").addClass("navbar-shrink") : $("#mainNav").removeClass("navbar-shrink")
+
+    $(".loader").fadeOut(700, function() {
+        $(".content").fadeIn(500); 
+        $(".loader").remove();
     });
-  
+ 
+    $(document).on('scroll', function(){
+        if ($("#mainNav").offset().top > 70) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else{
+            $("#mainNav").removeClass("navbar-shrink");
+            $('.navbar-collapse').collapse('hide');
+        }
+    });
+    
+    $(document).on('click', '.navbar-shrink a#home' ,function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
     $(document).on('mouseenter', '#map path, #map polyline,#map circle,#map polygon', function () {
         let m = $(this);
         if (m.attr('title')) {
             $(m).tooltip('show');
         }
     });
-    $.get('http://127.0.0.1:5500/ca.min.svg', function(svg){
+    $.get('https://arthurmelikyan.github.io/ca.min.svg', function(svg){
         $('#map .map_block').html(svg);
     }, 'text');
- 
+
+    $('.tt').tooltip();
+
+    $('#mainNav').on('show.bs.collapse', function () {
+        if (!$("#mainNav").hasClass('navbar-shrink') && $("#mainNav").offset().top < 80) {
+            $("#mainNav").addClass("navbar-shrink");
+        }
+      })
 });
