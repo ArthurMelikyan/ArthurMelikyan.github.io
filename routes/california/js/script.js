3d97cb7ea0f25a72403ad5354d11c91542fe4a4d
ArthurMelikyan <arthmelikyan@gmail.com>
2020-08-30 22:21:51 +0400

    coments

--- routes/california/js/script.js
+++ routes/california/js/script.js
@@ -4,35 +4,6 @@ $(document).ready(function(){
         $(".content").fadeIn(500); 
         $(".loader").remove();
     });
-    // function order_page_height(){
-    //     if ($(".order").length) {   
-    //         var header_h = $('.order .top_header').outerHeight(true);
-    //         var footer_h = $('.order #footer').outerHeight(true);
-    //         var calc_block_h = window.innerHeight-header_h-footer_h;
-    //         $('.first_order_msg_block').height(calc_block_h);
-    //         $('.order_title').css({marginTop:calc_block_h/4});
-    //     }
-    // }
-    // order_page_height();
-    // $(window).resize(function() { 
-    //     order_page_height();
-    // });
-    // window.onresize = function(e) {
-    //     order_page_height();
-    // };
-    // $(document).on('scroll', function(){
-    //     if ($("#mainNav").offset().top > 70) {
-    //         $("#mainNav").addClass("navbar-shrink");
-    //     } else{
-    //         $("#mainNav").removeClass("navbar-shrink");
-    //         $('.navbar-collapse').collapse('hide');
-    //     }
-    // });
-    
-    // $(document).on('click', '.navbar-shrink a#home' ,function(){
-    //     $("html, body").animate({ scrollTop: 0 }, "slow");
-    //     return false;
-    // });
 
     $(document).on('mouseenter', '#map path, #map polyline,#map circle,#map polygon', function () {
         let m = $(this);
